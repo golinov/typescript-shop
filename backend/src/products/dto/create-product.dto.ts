80911ce163984123780f0bc9154d35c73993912e
Alexandr <golinov95@gmail.com>
2021-06-04 13:41:10 +0300

    swagger desc

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,15 +1,36 @@
 import { IsIn, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { OriginType, originTypes } from '../entities/product.entity';
+import { Type } from 'class-transformer';
 
 export class CreateProductDto {
+  @ApiProperty({ example: 'my product', description: 'product name' })
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
+  @ApiProperty({
+    example: '99.99',
+    description: 'product price',
+    type: 'number',
+  })
+  @Type(() => Number)
   @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   readonly price: number;
 
+  @ApiProperty({
+    example: 'usa',
+    description: 'product origin',
+    type: 'string',
+    enum: originTypes,
+  })
   @IsNotEmpty()
-  @IsIn(['asia', 'europe', 'usa'])
+  @IsIn(originTypes)
   @IsString()
-  readonly origin: string;
+  readonly origin: OriginType;
+
+  @ApiProperty({ type: 'file' })
+  readonly picture: any;
 }
