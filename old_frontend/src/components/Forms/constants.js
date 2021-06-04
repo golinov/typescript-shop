e01c32efba51f2fe2cea73186bf522b677ee44af
golinov <68328829+golinov@users.noreply.github.com>
2021-06-05 00:33:29 +0300

    Delete old_frontend directory

--- old_frontend/src/components/Forms/constants.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const validationSchema = Yup.object().shape({
-	name: Yup.string().trim('name should not have spaces at start and end of string').strict(true).min(3).max(20).required("Required"),
-	price: Yup.number().typeError('price must be a positive number').integer().positive().required("Required"),
-	origin: Yup.string().trim().strict(true).required("Required")
-})
--- old_frontend/src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import contactReducer from './itemsReducer';
-
-export default combineReducers({
-	products: contactReducer,
-});
